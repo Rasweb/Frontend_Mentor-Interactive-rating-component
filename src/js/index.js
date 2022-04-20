293c3991369d4043ea80f85551fc2e3917ac3652
RasWeb <rasmuspalm@fastmail.se>
2022-04-20 11:07:46 +0200

    JavaScript rating done

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,25 @@
-window.onload = () => {
-  document.getElementById("submit").addEventListener("click", showDiv);
-};
+const submitBtn = document.getElementById("submit");
+const ratingBtns = document.querySelectorAll(".btn");
+let rating;
+
+ratingBtns.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    rating = btn.innerHTML;
+  });
+});
 
 function showDiv() {
   document.getElementById("ratingDiv").style.display = "none";
   document.getElementById("thankYouDiv").style.display = "block";
+  const doneRating = (document.getElementById(
+    "ratingDone"
+  ).innerHTML = `You selected ${rating} out of 5`);
 }
+
+submitBtn.addEventListener("click", () => {
+  if (rating > 0) {
+    showDiv();
+  } else {
+    console.log("Bad");
+  }
+});
